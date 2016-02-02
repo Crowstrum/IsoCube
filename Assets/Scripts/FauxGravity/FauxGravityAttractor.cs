308ca6cc1d5fa7912a8ffcc7a8975faaea7315af
Travis <crowstrum@gmail.com>
2016-02-01 20:47:31 -0600

    Init Commit

---
+++ Assets/Scripts/FauxGravity/FauxGravityAttractor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FauxGravityAttractor : MonoBehaviour {
+    float Gravity = -10f;
+
+	public void Attract(Transform body)
+    {
+        Vector3 gravityUp = (body.position - transform.position).normalized;
+        Vector3 bodyUp = body.up;
+
+        body.GetComponent<Rigidbody>().AddForce(gravityUp * Gravity);
+
+        Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * body.rotation;
+        body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 50f * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = transform;
+        }
+    }
+}
---
+++ Assets/Scripts/FauxGravity/FauxGravityBody.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FauxGravityBody : MonoBehaviour {
+    [SerializeField]
+    FauxGravityAttractor _attractor;
+    [SerializeField]
+    Transform[] points;
+    Transform _myTransform;
+
+
+	// Use this for initialization
+	void Start () {
+        _myTransform = transform;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        GetComponent<Rigidbody>().useGravity = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+      //  _attractor.Attract(_myTransform);
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            transform.position = points[1].position;
+        }
+	}
+}
